66aab1b6abeb03e7e292b837f1853e3e347849d3
Ayabdj <ayabboudjou@gmail.com>
2024-12-04 21:38:35 +0100

    Premier commit de mon projet React avec Tailwind CSS

---
+++ front-end/src/components/home/SectionOne.jsx
@@ -0,0 +1,9 @@
+const SectionOne = () => {
+    return (
+        <div className="bg-red-500 text-white flex justify-center items-center h-20">
+            <p>Hello</p>
+        </div>
+    );
+};
+
+export default SectionOne;
---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import SectionOne from "../components/home/SectionOne"
+const HomePage=()=>{
+    return(
+        <>
+         <SectionOne/>
+        </>
+    )
+}
+export default HomePage
